751ca0dddab609fd7843ccae86c94082f11e2c69
Hyeonrang <lhh1530@naver.com>
2023-04-10 21:19:38 +0900

    Initial commit

---
+++ app/src/main/java/com/cookandroid/project4_2/MainActivity.java
@@ -0,0 +1,143 @@
+package com.cookandroid.project4_2;
+
+import android.os.Bundle;
+
+import com.cookandroid.project4_2.R;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.text.Html;
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.cookandroid.project4_2.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView text1, text2;
+    CheckBox chkAgree;
+    RadioGroup rGroup1;
+    RadioButton rdoDog, rdoCat, rdoRabbit;
+    Button btnOK;
+    ImageView imgPet;
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("애완 동물 사진 보기");
+
+        text1 = (TextView) findViewById(R.id.Text1);
+        chkAgree = (CheckBox) findViewById(R.id.ChkAgree);
+
+        text2 = (TextView) findViewById(R.id.Text2);
+        rGroup1 = (RadioGroup) findViewById(R.id.Rgroup1);
+
+        rdoDog = (RadioButton) findViewById(R.id.RdoDog);
+        rdoCat = (RadioButton) findViewById(R.id.RdoCat);
+        rdoRabbit = (RadioButton) findViewById(R.id.RdoRabbit);
+
+        btnOK = (Button) findViewById(R.id.BtnOK);
+        imgPet = (ImageView) findViewById(R.id.ImgPet);
+
+        chkAgree.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b)
+            {
+                if (chkAgree.isChecked() == true)
+                {
+                    text2.setVisibility(android.view.View.VISIBLE);
+                    rGroup1.setVisibility(android.view.View.VISIBLE);
+                    btnOK.setVisibility(android.view.View.VISIBLE);
+                    imgPet.setVisibility(android.view.View.VISIBLE);
+                }
+                else
+                {
+                    text2.setVisibility(android.view.View.INVISIBLE);
+                    rGroup1.setVisibility(android.view.View.INVISIBLE);
+                    btnOK.setVisibility(android.view.View.INVISIBLE);
+                    imgPet.setVisibility(android.view.View.INVISIBLE);
+                }
+            }
+        });
+
+        btnOK.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View arg0)
+            {
+                switch (rGroup1.getCheckedRadioButtonId())
+                {
+                    case R.id.RdoDog:
+                        imgPet.setImageResource(R.drawable.dog);
+                        break;
+
+                    case R.id.RdoCat:
+                        imgPet.setImageResource(R.drawable.cat);
+                        break;
+
+                    case R.id.RdoRabbit:
+                        imgPet.setImageResource(R.drawable.rabbit);
+                        break;
+
+                    default:
+                        Toast.makeText(getApplicationContext(), "동물 먼저 선택하세요.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
